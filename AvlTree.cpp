760b5c15be74694520d2e8574abe22ad75d229e4
Benjamin Goodhart <bengoodhart16@gmail.com>
2022-01-16 18:06:35 -0500

    added rotate, getBalance, and insert functions

--- AvlTree.cpp
+++ AvlTree.cpp
@@ -47,3 +47,65 @@ Node* rightRotate(Node* oldRoot){
 }
 
 
+
+Node* leftRotate(Node* x) {
+    Node *y = x->right; 
+    Node *T2 = y->left; 
+
+    y->left = x; 
+    x->right = T2; 
+
+    x->height = max(height(x->left), height(x->right)) + 1; 
+    y->height = max(height(y->left), height(y->right)) + 1; 
+    return y;
+}
+
+
+
+int getBalance(Node* N) {
+    if(n == NULL)
+        return 0;
+    return height(N->left) - height(N->right);
+}
+
+
+
+Node* insert(int key, Node* node) {
+    if(node == NULL)
+        return(newNode(key));
+
+    if(key < node->key)
+        node->left = insert(node->left, key);
+    else if(key > node->key)
+        node->right = insert(node->right, key);
+    else
+        return node;
+
+
+    node->height = 1 + max(height(node->left), height(node->right)); 
+    
+    
+    int balance = getBalance(node);
+
+
+    if(balance > 1 && key < node->left->key)
+        return rightRotate(node);
+
+    if(balance < -1 && key > node->right->key)
+        return leftRotate(node);
+    
+    if(balance > 1 && key > node->left->key){
+        node->left - leftRotate(node->left);
+        return rightRotate(node);
+    }
+    if(balance < -1 && key < node->right->key){
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+
+
+    return node;
+
+}
+
+
