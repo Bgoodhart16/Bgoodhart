02e4c249a81af0aa4392c4b4b28d3f578e55d135
Benjamin Goodhart <bengoodhart16@gmail.com>
2022-02-25 12:52:46 -0500

    made it a lot better

--- JokeWebsite/script.js
+++ JokeWebsite/script.js
@@ -18,4 +18,8 @@ function displayJoke(data) {
 function displayError() {
     const error = document.getElementById("error");
     error.innerHTML = "Whoops, something went wrong. Please try again later!";
+}
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
   }
